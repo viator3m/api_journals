c88ec463b2f3d77acf99c9796852db2f2ac53d4b
viator3m <viator3m@gmail.com>
2022-06-05 13:41:48 +0300

    PEP8

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -5,7 +5,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-    path('redoc/', TemplateView.as_view(template_name='redoc.html'),
-         name='redoc'
-         ),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'), name='redoc'
+    ),
 ]
