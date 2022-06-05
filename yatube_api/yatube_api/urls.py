032dcf5176ac8f059106d7517bf5af6dcc142343
viator3m <viator3m@gmail.com>
2022-06-05 13:52:45 +0300

    PEP8

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
     path(
-         'redoc/',
-         TemplateView.as_view(template_name='redoc.html'), name='redoc'
-    ),
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'), name='redoc'),
 ]
