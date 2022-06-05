6a0c4444d47f74ad70b716297ae30da8bc91bb26
viator3m <viator3m@gmail.com>
2022-06-05 14:28:58 +0300

    PEP8

--- yatube_api/yatube_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
-    path('redoc/', TemplateView.as_view(template_name='redoc.html'), name='redoc'), # noqa
-]
