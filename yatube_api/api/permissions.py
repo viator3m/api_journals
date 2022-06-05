2211c1b7295b2a4b69f2a956a67f2df669dbd9c2
viator3m <viator3m@gmail.com>
2022-06-05 12:48:32 +0300

    delete unresolved files

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,8 +2,5 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAuthorOrReadOnly(BasePermission):
-    # def has_permission(self, request, view):
-    #     return request.method in SAFE_METHODS
-
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or obj.author == request.user
